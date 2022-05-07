b814f22bc075247f718490f2e8119ff54f8c19de
Melian <79233450803@yandex.ru>
2022-05-07 23:20:04 +0700

    сделала notification в System Tray для уведомления о сохранении заметки

--- app/src/main/java/com/geekbrains/mynotegb/BottomSheetDialogFragment.java
+++ app/src/main/java/com/geekbrains/mynotegb/BottomSheetDialogFragment.java
@@ -1,5 +1,8 @@
 package com.geekbrains.mynotegb;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,8 +10,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class BottomSheetDialogFragment extends com.google.android.material.bottomsheet.BottomSheetDialogFragment {
+
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        NotificationChannelCompat notificationChannelCompat = new NotificationChannelCompat.Builder(CHANNEL_ID, NotificationManagerCompat.IMPORTANCE_DEFAULT)
+                .setName("Заметка сохранена")
+                .build();
+
+        NotificationManagerCompat.from(requireContext())
+                .createNotificationChannel(notificationChannelCompat);
+    }
 
     @Nullable
     @Override
@@ -24,6 +44,19 @@ public class BottomSheetDialogFragment extends com.google.android.material.botto
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent(requireContext(), MainActivity.class);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(requireContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                Notification notification = new NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+                        .setContentTitle("Заметка сохранена")
+                        .setSmallIcon(R.drawable.ic_baseline_speaker_notes_24)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .build();
+
+                NotificationManagerCompat.from(requireContext())
+                        .notify(10, notification);
             }
         });
 
